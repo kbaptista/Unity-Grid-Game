12b77e85f5d0eda229d9c03f722b76e62b99f570
kbaptista <kevbap@gmail.com>
2016-10-28 17:45:36 +0200

    Evo

--- HeartGames-Exercie-1-1/Assets/scripts/Grid.cs
+++ HeartGames-Exercie-1-1/Assets/scripts/Grid.cs
@@ -7,17 +7,23 @@ public class Grid : MonoBehaviour {
 	private static int TARGET_VALUE = 10 ;
 	private static int TILE_MAXIMUM_VALUE = 9 ;
 
-	public GameObject[] grid ;
+	public Tile[] grid ;
+	public Tile prefab ;
 
-	public int grid_size = GRID_SIZE ;
+	public int grid_size = 0;
 
 	public void CreateGrid(){
+		if (grid_size == 0)
+			grid_size = GRID_SIZE * GRID_SIZE;
+		
+		grid = new Tile[grid_size];
+
 		for(int x_tilesCreated = 0 ; x_tilesCreated < grid_size ; x_tilesCreated++){
 			for(int y_tilesCreated = 0 ; y_tilesCreated < grid_size ; y_tilesCreated++){
 				int random_value = (int)(Random.value*(TILE_MAXIMUM_VALUE-1)+1);
-				string value =  random_value.ToString();
-				GameObject newTile = GameObject.CreatePrimitive(PrimitiveType.Cube);// (value, new Vector3(x_tilesCreated, 0.0f, y_tilesCreated));
+				Tile newTile = (Tile)Instantiate(prefab, new Vector3(x_tilesCreated*1.0f, 0.0f, y_tilesCreated*1.0f), new Quaternion());
 				TextMesh text = new TextMesh();
+				string value =  random_value.ToString();
 				text.text = value;
 				text.transform.parent = newTile.transform;
 				grid[x_tilesCreated+y_tilesCreated*grid_size] = newTile;
